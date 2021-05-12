c1f1ca1fde86a37f406c52e989ee4546f37c57d2
lehongquan15a <53887266+lehongquan15a@users.noreply.github.com>
2021-05-12 11:21:29 +0700

    add new image and fix some static page file

--- src/main/java/com/example/housewareshopproject/controller/MainController.java
+++ src/main/java/com/example/housewareshopproject/controller/MainController.java
@@ -1,12 +1,37 @@
 package com.example.housewareshopproject.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-    @RequestMapping("/")
-    public String home() {
+    @GetMapping("/")
+    public String index(){
         return "home";
+    }
+
+    @GetMapping("/products")
+    public String getListProducts(){
+        return "listProduct";
+    }
+
+    @GetMapping("/detail")
+    public String detail(){
+        return "detail";
+    }
+
+    @GetMapping("/carts")
+    public String getListCart(){
+        return "listCart";
+    }
+
+    @GetMapping("/checkout")
+    public String checkout(){
+        return "checkout";
+    }
+
+    @GetMapping("/prepare-shipping")
+    public String prepareShipping(){
+        return "prepareShipping";
     }
 }
